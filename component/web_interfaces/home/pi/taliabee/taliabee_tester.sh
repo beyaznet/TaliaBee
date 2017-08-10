194d074abe5b1cfdcd0fd18acf1776063f427afd
emrah <emrah.com@gmail.com>
2017-08-10 11:04:52 +0300

    optimize TaliaBee test script

--- component/web_interfaces/home/pi/taliabee/taliabee_tester.sh
+++ component/web_interfaces/home/pi/taliabee/taliabee_tester.sh
@@ -2,112 +2,101 @@
 
 DELAY=0.1
 
+function reset_on_exit {
+	curl -s "http://127.0.0.1/api/reset"
+}
+
+trap reset_on_exit EXIT
+
 while true
 do
 	# reset
-	curl "http://127.0.0.1/api/reset"
+	curl -s "http://127.0.0.1/api/reset"
 
 	# read di
 	for i in $(seq 1 2 16)
 	do
-		curl "http://127.0.0.1/api/di/$i/read"
+		curl -s "http://127.0.0.1/api/di/$i/read"
 		sleep $DELAY
 	done
 
 	# read do
 	for i in $(seq 1 2 12)
 	do
-		curl "http://127.0.0.1/api/do/$i/read"
+		curl -s "http://127.0.0.1/api/do/$i/read"
 		sleep $DELAY
 	done
 
 	# set do
 	for i in $(seq 1 12)
 	do
-		curl "http://127.0.0.1/api/do/$i/set"
+		curl -s "http://127.0.0.1/api/do/$i/set"
 		sleep $DELAY
 	done
 
 	# set relay / read relay
 	for i in $(seq 13 16)
 	do
-		curl "http://127.0.0.1/api/ro/$i/set"
-		curl "http://127.0.0.1/api/ro/$i/read"
+		curl -s "http://127.0.0.1/api/ro/$i/set"
+		curl -s "http://127.0.0.1/api/ro/$i/read"
 		sleep $DELAY
 	done
 
 	# read di
 	for i in $(seq 2 2 16)
 	do
-		curl "http://127.0.0.1/api/di/$i/read"
+		curl -s "http://127.0.0.1/api/di/$i/read"
 		sleep $DELAY
 	done
 
 	# read do
 	for i in $(seq 2 2 12)
 	do
-		curl "http://127.0.0.1/api/do/$i/read"
+		curl -s "http://127.0.0.1/api/do/$i/read"
 		sleep $DELAY
 	done
 
 	# reset do
 	for i in $(seq 1 12)
 	do
-		curl "http://127.0.0.1/api/do/$i/reset"
+		curl -s "http://127.0.0.1/api/do/$i/reset"
 		sleep $DELAY
 	done
 
 	# reset relay
 	for i in $(seq 13 16)
 	do
-		curl "http://127.0.0.1/api/ro/$i/reset"
-		curl "http://127.0.0.1/api/ro/$i/read"
+		curl -s "http://127.0.0.1/api/ro/$i/reset"
+		curl -s "http://127.0.0.1/api/ro/$i/read"
 		sleep $DELAY
 	done
 
-	# increment ao
-	for i in $(seq 0 50 4095)
-	do
-		curl "http://127.0.0.1/api/ao/1/write?val=$i"
-		curl "http://127.0.0.1/api/ao/2/write?val=$i"
-		curl "http://127.0.0.1/api/ao/3/write?val=$i"
-		curl "http://127.0.0.1/api/ao/4/write?val=$i"
-	done
+	# write ao
+	curl -s "http://127.0.0.1/api/ao/1/write?val=$((RANDOM % 4095 + 1))"
+	curl -s "http://127.0.0.1/api/ao/2/write?val=$((RANDOM % 4095 + 1))"
+	curl -s "http://127.0.0.1/api/ao/3/write?val=$((RANDOM % 4095 + 1))"
+	curl -s "http://127.0.0.1/api/ao/4/write?val=$((RANDOM % 4095 + 1))"
 
 	# read ai / ao
 	for i in $(seq 1 4)
 	do
-		curl "http://127.0.0.1/api/ai/$i/read"
-		curl "http://127.0.0.1/api/ao/$i/read"
-		sleep $DELAY
-	done
-
-	# decrement ao
-	for i in $(seq 4095 -50 0)
-	do
-		curl "http://127.0.0.1/api/ao/1/write?val=$i"
-		curl "http://127.0.0.1/api/ao/2/write?val=$i"
-		curl "http://127.0.0.1/api/ao/3/write?val=$i"
-		curl "http://127.0.0.1/api/ao/4/write?val=$i"
-	done
-
-	# write relay
-	for i in $(seq 13 16)
-	do
-		curl "http://127.0.0.1/api/ro/$i/write?val=1"
-		sleep $DELAY
-	done
-
-	# write do
-	for i in $(seq 1 12)
-	do
-		curl "http://127.0.0.1/api/do/$i/write?val=1"
+		curl -s "http://127.0.0.1/api/ai/$i/read"
+		curl -s "http://127.0.0.1/api/ao/$i/read"
 		sleep $DELAY
 	done
 
 	# read temperature
-	curl "http://127.0.0.1/api/temperature/read"
+	curl -s "http://127.0.0.1/api/temperature/read"
 
 	# status
-	curl "http://127.0.0.1/api/status"
+	curl -s "http://127.0.0.1/api/status"
+
+	# wait
+	for i in $(seq 1 20)
+	do
+	    V=$(($i % 2))
+	    curl -s "http://127.0.0.1/api/ro/13/write?val=$V"
+	    curl -s "http://127.0.0.1/api/do/4/write?val=$V"
+	    sleep 0.25
+	done
 done
